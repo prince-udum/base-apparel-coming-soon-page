19d7ba5907f85c8c34022c19a200717f213c4c6b
prince <princeudum1@gmail.com>
2023-08-02 14:22:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+const errorMsg = document.querySelector("#errorMsg");
+const email = document.querySelector(".email");
+const errorIcon = document.querySelector(".error-icon");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!email.value.includes("@")) {
+    console.log("hello", errorIcon);
+    email.style.border = "2px solid hsl(0, 93%, 68%)";
+    errorMsg.style.visibility = "visible";
+    errorIcon.style.visibility = "visible";
+
+    setTimeout(() => {
+      email.style.border = "1px solid hsl(0, 36%, 70%)";
+      errorMsg.style.visibility = "hidden";
+      errorIcon.style.visibility = "hidden";
+    }, 2000);
+  } else {
+    email.value = "";
+  }
+});
